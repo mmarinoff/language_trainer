8777ee5a07d36deded08051f0866edad34df0daf
Matthew Marinoff <matthew.marinoff@gmail.com>
2021-04-19 12:53:58 -0400

    tag scrape will now find correct start and stop indices for all tags, even recursive ones
    tag scan will return all instances of a specific tag within the html document

--- data_collection.py
+++ data_collection.py
@@ -20,10 +20,9 @@ class GetRequest:
     def __init__(self, content: requests.models.Response):
         # sort content into body and head
         self.content = content.text.splitlines()  # newline delimited html page
-        #self.head = self.tag_scrape(self.content, 'head')  # head
-        #self.body = self.tag_scrape(self.content, 'body', line_index=self.head.line_indices[0])  # body
+        self.head = self.tag_scrape(self.content, 'head')[0]  # head
+        self.body = self.tag_scrape(self.content, 'body', start_index=self.head.line_indices[0])[0]  # body
         self.div = self.tag_scrape(self.content, 'div')
-        print(self.div)
 
     def __str__(self, line_indices=None, tag_indices=None):
 
@@ -37,60 +36,76 @@ class GetRequest:
 
         return '\n'.join(subcontent)
 
-    def tag_scrape(self, content: list, tag: str, line_index=0, tag_index=0):
+    def tag_scrape(self, content: list, tag: str, start_index=0, tag_index=0):
 
         tags = ['<' + tag, '</' + tag + '>']  # open tag, close tag
 
-        # shift content to starting search index
-        subcontent = content[line_index:]  # starting line index
-        if tag_index:
-            subcontent[0] = "".join(re.split('(<[^>]*>)', content[0])[1::2][tag_index:])  # starting tag index
-
         state = 0  # open tag not detected yet
-        subtags = None
-        for i, line in enumerate(subcontent):  # read through html block line by line
+        subtags = []
+        line_index = start_index
+        while line_index < len(content):
+            line = content[line_index]
 
             # check for desired open/close tags in current line
             tag_detect = [tag in line for tag in tags]
             if any(tag_detect):  # desired tag detected
                 line_split = re.split('(<[^>]*>)', line)[1::2]  # break line into tags
 
-                if i == 0:  # absolute index stored, if first line apply shift to account for slicing above
-                    abs_tag = tag_index
-                else:  # not first line in selection
-                    abs_line, abs_tag = 0, 0
+                # if first line, start at tag index, else start at first element in line
+                if line_index == start_index:
+                    ls = tag_index
+                else:
+                    ls = 0
 
-                for j, ls in enumerate(line_split):
+                # loop through line sub-elements between <> braces, search for tag
+                while ls < len(line_split):
+                    subline = line_split[ls]
 
-                    # open tag detected in line
-                    if tags[0] in ls:
+                    # open tag detected in subline
+                    if tags[0] in subline:
+                        # cl = re.search('.*=".*"', subline)
+
                         if state == 0:  # first open tag detected, store index
-                            start_line_index = line_index + i
-                            start_tag_index = abs_tag + j
-
+                            start_line_index = line_index
+                            start_tag_index = ls
                             state = 1  # in case of nested tags
-                        else:  # second open tag detected prior to closing tag, recursively call self
-                            subtags = self.tag_scrape(content, tag, start_line_index, start_tag_index + 1)
-                            # continue search for closing tag after subtag
 
+                        else:  # second open tag detected prior to closing tag, recursively call self
+                            subtags = subtags + self.tag_scrape(content, tag, line_index, ls)
+
+                            line_index = subtags[-1].line_indices[1]
+                            ls = subtags[-1].tag_indices[1]
+                            # continue search for closing tag after subtag
 
                     # close tag detected in line
                     if tag_detect[1] and state == 1:  # create Tag class object to store information
-                        end_line_index = line_index + i
-                        end_tag_index = abs_tag + j
-                        tag_instance = self.Tag(self, line_indices=[start_line_index, end_line_index],
-                                                tag_indices=[start_tag_index, end_tag_index])
+                        end_line_index = line_index
+                        end_tag_index = ls
+                        tag_instance = self.Tag(self, line_indices=(start_line_index, end_line_index),
+                                                tag_indices=(start_tag_index, end_tag_index))
 
+                        if subtags:
+                            return [tag_instance] + subtags
+                        else:
+                            return [tag_instance]
 
-                        if subtags:  # return recurred tags as list
-                            if type(subtags) == self.Tag:
-                                subtags = [subtags]
-                            subtags.append(tag_instance)
-                            return subtags
-                        else:  # return single tag
-                            return tag_instance
+                    ls += 1
+            line_index += 1
 
-        raise LineIndexError('content', str('Tag ' + tags[state] + ' Not found in selection'))
+    def tag_scan(self, content: list, tag: str, start_index=0, tag_index=0):
+
+        line_index = start_index
+        tagslist = []
+        tags = True  # do at least one cycle
+        while tags:
+            tags = self.tag_scrape(self.content, tag, line_index, tag_index)
+
+            if tags:
+                tagslist = tagslist + tags
+                tag_index = tags[0].tag_indices[1]+1
+                line_index = tags[0].line_indices[1]
+
+        return tagslist
 
     @staticmethod
     def tag_split(line, tag_index):
@@ -107,5 +122,7 @@ class GetRequest:
 
 
 gr = GetRequest(r)
-for tag in gr.div:
-    print(tag)
+abba = gr.tag_scan(gr.content, 'div')
+
+
+
